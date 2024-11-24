59212df756de8ed0837e87c2395fea1bcf95bda0
Kevin Thomas <kevin.jt2007@gmail.com>
2024-11-24 12:30:55 -0800

    fix: update moveplot even if sf depth = 18
    thanks @jamma from discord!

--- src/pages/analysis/[...id].tsx
+++ src/pages/analysis/[...id].tsx
@@ -288,24 +288,24 @@ const Analysis: React.FC<Props> = ({
         }
       }
 
-      const evals = game.maiaEvaluations[model]
-      if (evals) {
-        const newAnalyzedGame = { ...analyzedGame }
-        newAnalyzedGame.maiaEvaluations[model] = evals
-
-        for (let i = 0; i < analyzedGame.moves.length; i++) {
-          const maiaValues = newAnalyzedGame.moves[i].maia_values
-          if (maiaValues) {
-            const newMaiaValues = game.moves[i].maia_values as {
-              [key: string]: number
-            }
-            maiaValues[model] = newMaiaValues[model]
-          }
-        }
-
-        setAnalyzedGame(newAnalyzedGame)
-        setCurrentMaiaModel(model)
+      const newAnalyzedGame = { ...analyzedGame }
+      newAnalyzedGame.maiaEvaluations = {
+        ...newAnalyzedGame.maiaEvaluations,
+        ...game.maiaEvaluations,
       }
+
+      for (let i = 0; i < analyzedGame.moves.length; i++) {
+        const maiaValues = newAnalyzedGame.moves[i].maia_values
+        if (maiaValues) {
+          const newMaiaValues = game.moves[i].maia_values as {
+            [key: string]: number
+          }
+          maiaValues[model] = newMaiaValues[model]
+        }
+      }
+
+      setAnalyzedGame(newAnalyzedGame)
+      setCurrentMaiaModel(model)
     }
   }
 
