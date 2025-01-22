31bbc4afe1bb277b1e0570cd10f22bfa226384a7
Kevin Thomas <kevin.jt2007@gmail.com>
2025-01-22 14:15:51 -0800

    feat: change path urls from /analyze to /analysis and port /analysis to /analysis/legacy

--- src/components/Analysis/ConfigureAnalysis.tsx
+++ src/components/Analysis/ConfigureAnalysis.tsx
@@ -22,7 +22,7 @@ export const ConfigureAnalysis: React.FC<Props> = ({
         <p className="text-sm text-secondary">Analyze using:</p>
         <select
           value={currentMaiaModel}
-          className="cursor-pointer rounded border-none bg-human-4/40 p-2 text-primary/70 outline-none transition duration-300 hover:bg-human-4/60 hover:text-primary"
+          className="cursor-pointer rounded border-none bg-human-4/60 p-2 text-primary/70 outline-none transition duration-300 hover:bg-human-4/80 hover:text-primary"
           onChange={(e) => setCurrentMaiaModel(e.target.value)}
         >
           {MAIA_MODELS.map((model) => (
@@ -34,12 +34,12 @@ export const ConfigureAnalysis: React.FC<Props> = ({
       </div>
       <ContinueAgainstMaia
         launchContinue={launchContinue}
-        background="bg-human-4/40 hover:bg-human-4/60 text-primary/70 hover:text-primary"
+        background="bg-human-4/60 hover:bg-human-4/80 text-primary/70 hover:text-primary"
       />
       <p className="mt-2 text-sm text-secondary">
         If you are having performance issues, you can switch to our legacy{' '}
         <Link
-          href={window.location.href.replace('/analyze', '/analysis')}
+          href={window.location.href.replace('/analysis', '/analysis/legacy')}
           className="text-primary/80 underline transition duration-200 hover:text-primary/100"
         >
           Analysis Lite
