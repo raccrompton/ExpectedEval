e19fa179634f35dd6cc629c0b1aa9e87d99f8db0
Kevin Thomas <kevin.jt2007@gmail.com>
2025-02-03 22:08:06 -0800

    feat: port to new endpoints + move towards adding modal before using maia2

--- src/hooks/useMaiaEngine/useMaiaEngine.ts
+++ src/hooks/useMaiaEngine/useMaiaEngine.ts
@@ -1,12 +1,18 @@
-import { useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 
 import Maia from 'src/utils/maia2'
 
 export const useMaiaEngine = () => {
-  const maia = useMemo(() => {
-    const model = new Maia({ model: '/maia2/maia_rapid.onnx' })
-    return model
+  const [maia, setMaia] = useState<Maia>()
+
+  useEffect(() => {
+    setMaia(new Maia({ model: '/maia2/maia_rapid.onnx', type: 'rapid' }))
   }, [])
+
+  // const maia = useMemo(() => {
+  //   const model = new Maia({ model: '/maia2/maia_rapid.onnx', type: 'rapid' })
+  //   return model
+  // }, [])
 
   return maia
 }
