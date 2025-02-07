1c91d499b25176b6e5ae85cc002a1c14c7863c85
Kevin Thomas <kevin.jt2007@gmail.com>
2025-02-07 14:56:53 -0800

    feat: create copy of analysis via /analysis/client pages

---
+++ src/pages/analysis/client/index.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useContext, useEffect } from 'react'
+
+import { Loading } from 'src/components'
+import { AnalysisListContext } from 'src/contexts'
+
+const AnalysisPage: NextPage = () => {
+  const { push } = useRouter()
+  const { analysisTournamentList } = useContext(AnalysisListContext)
+
+  useEffect(() => {
+    if (analysisTournamentList) {
+      const entries = Array.from(analysisTournamentList.entries()).sort(
+        (a, b) => b[0].split('---')[1].localeCompare(a[0].split('---')[1]),
+      )
+      const [firstPart, games] = entries[0]
+      const gameId = firstPart.split('---')[0] + '/' + games[0].game_index
+      push('/analysis/client/' + gameId)
+    }
+  }, [analysisTournamentList, push])
+
+  return <Loading />
+}
+
+export default AnalysisPage
