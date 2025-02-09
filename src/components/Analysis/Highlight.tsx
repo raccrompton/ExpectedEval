c63fd3f3e37f5272939c09f056640c1dad869717
Kevin Thomas <kevin.jt2007@gmail.com>
2025-02-09 14:37:02 -0800

    feat: moved moves by rating graph to highlight component + added move recommendations to current position component

--- src/components/Analysis/Highlight.tsx
+++ src/components/Analysis/Highlight.tsx
@@ -3,6 +3,7 @@ import { BlunderMeter } from './BlunderMeter'
 import { MovesByRating } from './MovesByRating'
 
 interface Props {
+  currentMaiaModel: string
   moveEvaluation: {
     maia?: MaiaEvaluation
     stockfish?: StockfishEvaluation
@@ -13,6 +14,10 @@ interface Props {
       color: string
     }
   }
+  recommendations: {
+    maia?: { move: string; prob: number }[]
+    stockfish?: { move: string; cp: number }[]
+  }
   blunderMeter: {
     blunderMoveChance: number
     okMoveChance: number
@@ -22,39 +27,35 @@ interface Props {
 }
 
 export const Highlight: React.FC<Props> = ({
+  currentMaiaModel,
+  recommendations,
   blunderMeter,
   moveEvaluation,
   colorSanMapping,
   movesByRating,
 }: Props) => {
   return (
-    <div className="flex h-full w-full items-center overflow-hidden rounded border-[0.5px] border-white/40 bg-background-1">
-      <div className="flex w-1/2 flex-col border-r-[0.5px] border-white/40">
-        <div className="flex flex-col gap-0.5 p-3">
-          <p className="text-lg font-semibold">Current Position</p>
-          <p className="text-xs text-secondary">
-            Maia predicts that Black will play{' '}
-            {moveEvaluation?.maia
-              ? (colorSanMapping[Object.keys(moveEvaluation.maia.policy)[0]]
-                  ?.san ?? Object.keys(moveEvaluation.maia.policy)[0])
-              : '...'}{' '}
-            next. This is a blunder.
-          </p>
-        </div>
+    <div className="flex h-full w-full items-start overflow-hidden rounded border-[0.5px] border-white/40 bg-background-1">
+      <div className="flex h-full min-w-[40%] flex-col border-r-[0.5px] border-white/40">
         <div className="grid grid-cols-2">
-          <div className="flex flex-col items-center justify-center bg-human-3/5 py-2">
-            <p className="text-xs text-human-2">Maia White Win %</p>
-            <p className="text-xl font-bold text-human-1">
+          <div className="flex flex-col items-center justify-center gap-1 bg-human-3/5 py-3">
+            <p className="text-center text-sm text-human-2">
+              Maia {currentMaiaModel.slice(-4)}
+              <br />
+              White Win %
+            </p>
+            <p className="text-2xl font-bold text-human-1">
               {moveEvaluation?.maia
                 ? `${Math.round(moveEvaluation.maia?.value * 1000) / 10}%`
                 : '...'}
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center bg-engine-3/5 py-2">
-            <p className="text-sm text-engine-2">
+          <div className="flex flex-col items-center justify-center gap-1 bg-engine-3/5 py-3">
+            <p className="text-center text-sm text-engine-2">
               SF Eval
+              <br />
               {moveEvaluation?.stockfish?.depth
-                ? ` (D${moveEvaluation.stockfish?.depth})`
+                ? ` (Depth ${moveEvaluation.stockfish?.depth})`
                 : ''}
             </p>
             <p className="text-2xl font-bold text-engine-1">
@@ -64,11 +65,56 @@ export const Highlight: React.FC<Props> = ({
             </p>
           </div>
         </div>
-        <div className="flex flex-col p-3">
-          <BlunderMeter {...blunderMeter} />
+        <div className="grid grid-cols-2">
+          <div className="grid grid-rows-2 items-center justify-center p-3">
+            {recommendations.maia?.map(({ move, prob }, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex cursor-default items-center justify-start gap-3 hover:underline"
+                  style={{
+                    color: colorSanMapping[move]?.color ?? '#fff',
+                  }}
+                >
+                  <p className="w-[42px] text-right font-mono text-sm">
+                    {Math.round(prob * 1000) / 10}%
+                  </p>
+                  <p className="text-left font-mono text-sm">
+                    {colorSanMapping[move]?.san ?? move}
+                  </p>
+                </div>
+              )
+            })}
+          </div>
+          <div className="grid grid-rows-2 flex-col items-center justify-center p-3">
+            {recommendations.stockfish?.map(({ move, cp }, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex cursor-default items-center justify-start gap-3 hover:underline"
+                  style={{
+                    color: colorSanMapping[move]?.color ?? '#fff',
+                  }}
+                >
+                  <p className="w-[42px] text-right font-mono text-sm">
+                    {cp > 0 ? '+' : null}
+                    {cp / 100}
+                  </p>
+                  <p className="font-mono text-sm">
+                    {colorSanMapping[move]?.san ?? move}
+                  </p>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
-      <div className="flex flex-col"></div>
+      <div className="flex h-full w-full flex-col">
+        <MovesByRating
+          moves={movesByRating}
+          colorSanMapping={colorSanMapping}
+        />
+      </div>
     </div>
   )
 }
--- src/components/Analysis/MovesByRating.tsx
+++ src/components/Analysis/MovesByRating.tsx
@@ -26,162 +26,163 @@ export const MovesByRating: React.FC<Props> = ({
   return (
     <div className="flex h-full w-full flex-col rounded bg-background-1/60">
       <p className="p-3 text-lg text-white">Moves by Rating</p>
-      <div className="flex h-full w-full flex-col">
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart data={moves} margin={{ left: -5, right: 0, bottom: 0 }}>
-            <CartesianGrid strokeDasharray="3 3" stroke="#3C3C3C" />
-            <XAxis
-              dataKey="rating"
-              axisLine={false}
-              tick={{
-                fill: 'white',
-                fontSize: 10,
-              }}
-              tickMargin={4}
-              ticks={[1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900]}
-            />
-            <YAxis
-              yAxisId="left"
-              orientation="left"
-              axisLine={false}
-              domain={[0, 100]}
-              tick={{
-                fill: 'white',
-                fontSize: 10,
-              }}
-              label={{
-                value: 'Maia Probability',
-                angle: -90,
-                fill: '#FE7F6D',
-                position: 'insideLeft',
-                dy: 46,
-                offset: 20,
-                fontWeight: 600,
-                fontSize: 14,
-              }}
-              tickCount={4}
-              tickMargin={2}
-              tickLine={false}
-              tickFormatter={(value) => `${value}%`}
-            />
-            <YAxis
-              yAxisId="right"
-              orientation="right"
-              axisLine={false}
-              domain={[0, 100]}
-              tick={{
-                fill: 'white',
-                fontSize: 10,
-              }}
-              tickCount={4}
-              tickMargin={5}
-              tickLine={false}
-              tickFormatter={(value) => `${value}%`}
-            />
-            <defs>
-              {moves &&
-                Object.keys(moves[0]).map((move, i) => {
-                  if (move === 'rating') {
-                    return null
-                  }
-                  return (
-                    <linearGradient
-                      key={`color${move}`}
-                      id={`color${move}`}
-                      x1="0"
-                      y1="0"
-                      x2="0"
-                      y2="1"
-                    >
-                      <stop
-                        offset="5%"
-                        stopColor={colorSanMapping[move]?.color ?? '#fff'}
-                        stopOpacity={0.5}
-                      />
-                      <stop
-                        offset="95%"
-                        stopColor={colorSanMapping[move]?.color ?? '#fff'}
-                        stopOpacity={0}
-                      />
-                    </linearGradient>
-                  )
-                })}
-            </defs>
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          data={moves}
+          width={200}
+          height={200}
+          margin={{ left: 0, right: 0, bottom: 0 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" stroke="#3C3C3C" />
+          <XAxis
+            dataKey="rating"
+            axisLine={false}
+            tick={{
+              fill: 'white',
+              fontSize: 10,
+            }}
+            tickMargin={4}
+            ticks={[1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900]}
+          />
+          <YAxis
+            yAxisId="left"
+            orientation="left"
+            axisLine={false}
+            domain={[0, 60]}
+            tick={{
+              fill: 'white',
+              fontSize: 10,
+            }}
+            label={{
+              value: 'Maia Probability',
+              angle: -90,
+              fill: '#FE7F6D',
+              position: 'insideLeft',
+              dy: 46,
+              offset: 20,
+              fontWeight: 600,
+              fontSize: 14,
+            }}
+            tickCount={4}
+            tickMargin={2}
+            tickLine={false}
+            tickFormatter={(value) => `${value}%`}
+          />
+          <YAxis
+            yAxisId="right"
+            orientation="right"
+            axisLine={false}
+            domain={[0, 60]}
+            tick={{
+              fill: 'white',
+              fontSize: 10,
+            }}
+            tickCount={4}
+            tickMargin={5}
+            tickLine={false}
+            tickFormatter={(value) => `${value}%`}
+          />
+          <defs>
             {moves &&
               Object.keys(moves[0]).map((move, i) => {
                 if (move === 'rating') {
                   return null
                 }
                 return (
-                  <Area
-                    key={i}
-                    yAxisId="left"
-                    dataKey={move}
-                    dot={{
-                      r: 3,
-                      stroke: colorSanMapping[move].color,
-                      strokeWidth: 3,
-                    }}
-                    stroke={colorSanMapping[move].color}
-                    fill={`url(#color${move})`}
-                    strokeWidth={3}
-                  />
+                  <linearGradient
+                    key={`color${move}`}
+                    id={`color${move}`}
+                    x1="0"
+                    y1="0"
+                    x2="0"
+                    y2="1"
+                  >
+                    <stop
+                      offset="5%"
+                      stopColor={colorSanMapping[move]?.color ?? '#fff'}
+                      stopOpacity={0.5}
+                    />
+                    <stop
+                      offset="95%"
+                      stopColor={colorSanMapping[move]?.color ?? '#fff'}
+                      stopOpacity={0}
+                    />
+                  </linearGradient>
                 )
               })}
-            <Tooltip
-              content={({ payload }) => {
-                return (
-                  <div className="flex w-32 flex-col rounded border border-white/10 bg-background-1 pb-2">
-                    <div className="flex px-3 py-2">
-                      {payload ? (
-                        <p className="text-sm">{payload[0]?.payload.rating}</p>
-                      ) : null}
-                    </div>
-                    {payload?.map((point) => {
-                      const san = colorSanMapping[point.name as string].san
-                      const prob = Math.round((point.value as number) * 10) / 10
-                      return (
-                        <div
-                          key={san}
-                          className="flex items-center justify-between px-3"
-                        >
-                          <p
-                            style={{
-                              color:
-                                colorSanMapping[point.name as string].color,
-                            }}
-                            className="text-xs"
-                          >
-                            {san}
-                          </p>
-                          <p
-                            style={{
-                              color:
-                                colorSanMapping[point.name as string].color,
-                            }}
-                            className="font-mono text-xs"
-                          >
-                            {prob}%
-                          </p>
-                        </div>
-                      )
-                    })}
+          </defs>
+          {moves &&
+            Object.keys(moves[0]).map((move, i) => {
+              if (move === 'rating') {
+                return null
+              }
+              return (
+                <Area
+                  key={i}
+                  yAxisId="left"
+                  dataKey={move}
+                  dot={{
+                    r: 3,
+                    stroke: colorSanMapping[move].color,
+                    strokeWidth: 3,
+                  }}
+                  stroke={colorSanMapping[move].color}
+                  fill={`url(#color${move})`}
+                  strokeWidth={3}
+                />
+              )
+            })}
+          <Tooltip
+            content={({ payload }) => {
+              return (
+                <div className="flex w-32 flex-col rounded border border-white/10 bg-background-1 pb-2">
+                  <div className="flex px-3 py-2">
+                    {payload ? (
+                      <p className="text-sm">{payload[0]?.payload.rating}</p>
+                    ) : null}
                   </div>
-                )
-              }}
-            />
-            <Legend
-              align="right"
-              verticalAlign="top"
-              wrapperStyle={{ top: -14, right: 20, fontSize: 14 }}
-              iconSize={0}
-              formatter={(value) => {
-                return colorSanMapping[value as string].san
-              }}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
+                  {payload?.map((point) => {
+                    const san = colorSanMapping[point.name as string].san
+                    const prob = Math.round((point.value as number) * 10) / 10
+                    return (
+                      <div
+                        key={san}
+                        className="flex items-center justify-between px-3"
+                      >
+                        <p
+                          style={{
+                            color: colorSanMapping[point.name as string].color,
+                          }}
+                          className="text-xs"
+                        >
+                          {san}
+                        </p>
+                        <p
+                          style={{
+                            color: colorSanMapping[point.name as string].color,
+                          }}
+                          className="font-mono text-xs"
+                        >
+                          {prob}%
+                        </p>
+                      </div>
+                    )
+                  })}
+                </div>
+              )
+            }}
+          />
+          <Legend
+            align="right"
+            verticalAlign="top"
+            wrapperStyle={{ top: -14, right: 20, fontSize: 14 }}
+            iconSize={0}
+            formatter={(value) => {
+              return colorSanMapping[value as string].san
+            }}
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   )
 }
