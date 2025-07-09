ff5530e35236b12ef5cb8b421a6ec60529859cee
Kevin Thomas <kevin.jt2007@gmail.com>
2025-07-09 15:29:07 -0400

    fix: tours in h&b and play page

--- src/contexts/ModalContext/ModalContext.ts
+++ src/contexts/ModalContext/ModalContext.ts
@@ -8,20 +8,10 @@ const fn = () => {
 }
 
 interface IModalContext {
-  openedModals: Modals
-  setOpenedModals: (arg0: Modals) => void
   playSetupModalProps?: ComponentProps<typeof PlaySetupModal>
   setPlaySetupModalProps: (arg0?: ComponentProps<typeof PlaySetupModal>) => void
 }
 
 export const ModalContext = React.createContext<IModalContext>({
-  openedModals: {
-    againstMaia: false,
-    handAndBrain: false,
-    analysis: false,
-    train: false,
-    turing: false,
-  },
-  setOpenedModals: fn,
   setPlaySetupModalProps: fn,
 })
--- src/providers/ModalContextProvider/ModalContextProvider.tsx
+++ src/providers/ModalContextProvider/ModalContextProvider.tsx
@@ -1,7 +1,5 @@
 import { ComponentProps, ReactNode, useState } from 'react'
 
-import { Modals } from 'src/types'
-import { useLocalStorage } from 'src/hooks'
 import { ModalContext } from 'src/contexts'
 import { PlaySetupModal } from 'src/components'
 
@@ -10,14 +8,6 @@ export const ModalContextProvider: React.FC<{ children: ReactNode }> = ({
 }: {
   children: ReactNode
 }) => {
-  const [modals, setModals] = useLocalStorage<Modals>('client:modals', {
-    againstMaia: false,
-    handAndBrain: false,
-    analysis: false,
-    train: false,
-    turing: false,
-  })
-
   const [playSetupModalProps, setPlaySetupModalProps] = useState<
     ComponentProps<typeof PlaySetupModal> | undefined
   >(undefined)
@@ -25,14 +15,6 @@ export const ModalContextProvider: React.FC<{ children: ReactNode }> = ({
   return (
     <ModalContext.Provider
       value={{
-        openedModals: {
-          againstMaia: modals.againstMaia,
-          handAndBrain: modals.handAndBrain,
-          analysis: modals.analysis,
-          train: modals.train,
-          turing: modals.turing,
-        },
-        setOpenedModals: setModals,
         playSetupModalProps,
         setPlaySetupModalProps,
       }}
