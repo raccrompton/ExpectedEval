94ec551903c67b72c84f47242d5c5871f0b2cd73
Kevin Thomas <kevin.jt2007@gmail.com>
2025-07-15 00:49:00 -0400

    feat: autoscroll to target element during walkthrough on mobile
    Resolves #66

--- src/components/Common/FeedbackButton.tsx
+++ src/components/Common/FeedbackButton.tsx
@@ -11,7 +11,7 @@ export const FeedbackButton: React.FC = () => {
   return (
     <button
       id="feedback-button"
-      className="fixed bottom-6 right-6 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-human-4 transition-all duration-200 hover:scale-105 hover:bg-human-3"
+      className="fixed bottom-6 right-6 z-10 flex h-12 w-12 items-center justify-center rounded-full bg-human-4 transition-all duration-200 hover:scale-105 hover:bg-human-3"
     >
       <span className="material-symbols-outlined text-white">feedback</span>
     </button>
