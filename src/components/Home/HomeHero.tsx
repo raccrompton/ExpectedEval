a950d406703f0c16bb687fc24f8b0fa206e1732d
Kevin Thomas <kevin.jt2007@gmail.com>
2025-08-09 01:06:06 -0400

    feat: add broadcasts to home page

--- src/components/Home/HomeHero.tsx
+++ src/components/Home/HomeHero.tsx
@@ -18,7 +18,6 @@ import { PlayType } from 'src/types'
 import { getGlobalStats, getActiveUserCount } from 'src/api'
 import { AuthContext, ModalContext } from 'src/contexts'
 import { AnimatedNumber } from 'src/components/Common/AnimatedNumber'
-import { LiveChessBoard } from 'src/components/Home/LiveChessBoard'
 
 interface Props {
   scrollHandler: () => void
@@ -296,7 +295,6 @@ export const HomeHero: React.FC<Props> = ({ scrollHandler }: Props) => {
             <></>
           )}
         </motion.div>
-        <LiveChessBoard />
       </div>
     </Fragment>
   )
